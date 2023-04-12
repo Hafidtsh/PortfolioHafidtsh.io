75e4fbdcf2be0ebf21ea07b5b1c8828b5498cc25
devziad2021 <devziad2021@gmail.com>
2023-04-12 14:39:02 +0700

    first commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -28,11 +28,8 @@ const Intro = () => {
         <div className="i-name">
           {/* yahan change hy darkmode ka */}
           <span style={{ color: darkMode ? "white" : "" }}>Hy! I Am</span>
-          <span>Andrew Thomas</span>
-          <span>
-            Frontend Developer with high level of experience in web designing
-            and development, producting the Quality work
-          </span>
+          <span>Hafid Triansyah</span>
+          <span>Frontend Developer with high level of experience in web designing and development, producting the Quality work</span>
         </div>
         <Link to="contact" smooth={true} spy={true}>
           <button className="button i-button">Hire me</button>
@@ -48,32 +45,17 @@ const Intro = () => {
       <div className="i-right">
         <img src={Vector1} alt="" />
         <img src={Vector2} alt="" />
-        <img src={boy} alt="" />
-        {/* animation */}
-        <motion.img
-          initial={{ left: "-36%" }}
-          whileInView={{ left: "-24%" }}
-          transition={transition}
-          src={glassesimoji}
-          alt=""
-        />
+        <img src={boy} alt="" style={{ width: "250px", borderRadius: "40px" }} />
 
-        <motion.div
-          initial={{ top: "-4%", left: "74%" }}
-          whileInView={{ left: "68%" }}
-          transition={transition}
-          className="floating-div"
-        >
+        {/* animation */}
+        <motion.img initial={{ left: "-36%" }} whileInView={{ left: "-24%" }} transition={transition} src={glassesimoji} alt="" />
+
+        <motion.div initial={{ top: "-4%", left: "74%" }} whileInView={{ left: "68%" }} transition={transition} className="floating-div">
           <FloatinDiv img={crown} text1="Web" text2="Developer" />
         </motion.div>
 
         {/* animation */}
-        <motion.div
-          initial={{ left: "9rem", top: "18rem" }}
-          whileInView={{ left: "0rem" }}
-          transition={transition}
-          className="floating-div"
-        >
+        <motion.div initial={{ left: "9rem", top: "18rem" }} whileInView={{ left: "0rem" }} transition={transition} className="floating-div">
           {/* floatinDiv mein change hy dark mode ka */}
           <FloatinDiv img={thumbup} text1="Best Design" text2="Award" />
         </motion.div>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -6,7 +6,7 @@ import Glasses from "../../img/glasses.png";
 import Humble from "../../img/humble.png";
 import { themeContext } from "../../Context";
 import { motion } from "framer-motion";
-import Resume from './resume.pdf';
+import Resume from "./resume2.pdf";
 
 const Services = () => {
   // context
@@ -39,48 +39,18 @@ const Services = () => {
       {/* right */}
       <div className="cards">
         {/* first card */}
-        <motion.div
-          initial={{ left: "25rem" }}
-          whileInView={{ left: "14rem" }}
-          transition={transition}
-        >
-          <Card
-            emoji={HeartEmoji}
-            heading={"Design"}
-            detail={"Figma, Sketch, Photoshop, Adobe Illustrator, Adobe xd"}
-          />
+        <motion.div initial={{ left: "25rem" }} whileInView={{ left: "14rem" }} transition={transition}>
+          <Card emoji={HeartEmoji} heading={"Design"} detail={"Figma, Sketch, Photoshop, Adobe Illustrator, Adobe xd"} />
         </motion.div>
         {/* second card */}
-        <motion.div
-          initial={{ left: "-11rem", top: "12rem" }}
-          whileInView={{ left: "-4rem" }}
-          transition={transition}
-        >
-          <Card
-            emoji={Glasses}
-            heading={"Developer"}
-            detail={"Html, Css, JavaScript, React, Nodejs, Express"}
-          />
+        <motion.div initial={{ left: "-11rem", top: "12rem" }} whileInView={{ left: "-4rem" }} transition={transition}>
+          <Card emoji={Glasses} heading={"Developer"} detail={"Html, Css, JavaScript, React, Nodejs, Express"} />
         </motion.div>
         {/* 3rd */}
-        <motion.div
-          initial={{ top: "19rem", left: "25rem" }}
-          whileInView={{ left: "12rem" }}
-          transition={transition}
-        >
-          <Card
-            emoji={Humble}
-            heading={"UI/UX"}
-            detail={
-              "Lorem ispum dummy text are usually use in section where we need some random text"
-            }
-            color="rgba(252, 166, 31, 0.45)"
-          />
+        <motion.div initial={{ top: "19rem", left: "25rem" }} whileInView={{ left: "12rem" }} transition={transition}>
+          <Card emoji={Humble} heading={"UI/UX"} detail={"Lorem ispum dummy text are usually use in section where we need some random text"} color="rgba(252, 166, 31, 0.45)" />
         </motion.div>
-        <div
-          className="blur s-blur2"
-          style={{ background: "var(--purple)" }}
-        ></div>
+        <div className="blur s-blur2" style={{ background: "var(--purple)" }}></div>
       </div>
     </div>
   );
